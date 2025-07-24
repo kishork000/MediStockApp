b3d5a1aa8e100aeb8e6c065345d59e59659d7746
kishork000 <kishor20x@gmail.com>
2025-07-24 11:58:11 +0530

    Update firebase-config.ts

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -5,9 +5,9 @@ import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   projectId: "medistock-manager-s729t",
-  appId: "1:1078125691458:web:0c84d0d68b64c3816623db",
-  storageBucket: "medistock-manager-s729t.firebasestorage.app",
-  apiKey: "AIzaSyCZysJi4N6BK7a59gmZDWjA3sdmp1PRbuA",
+  appId: "demo",
+  storageBucket: "demo",
+  apiKey: "demo",
   authDomain: "medistock-manager-s729t.firebaseapp.com",
   measurementId: "",
   messagingSenderId: "1078125691458",
